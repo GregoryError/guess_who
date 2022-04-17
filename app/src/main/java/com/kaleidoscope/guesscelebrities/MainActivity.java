4269777d1b78546febac14d383dda1e1d4d3e476
GregoryError <kolesnikoff2085@yandex.ru>
2022-04-17 22:19:19 +0300

    play() and final logic

--- app/src/main/java/com/kaleidoscope/guesscelebrities/MainActivity.java
+++ app/src/main/java/com/kaleidoscope/guesscelebrities/MainActivity.java
@@ -3,18 +3,25 @@ package com.kaleidoscope.guesscelebrities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.content.AsyncTaskLoader;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedList;
@@ -34,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private final String urlStr = "https://www.usmagazine.com/celebrities/a/";
     private String contentStr = "";
     HashMap<String, String> personMap;
+    private int choice = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,32 +63,54 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        play();
+
+    }
+
+
+    private void play() {
+
         personMap = new HashMap<>();
 
         parseToMap(contentStr, personMap);
 
         LinkedList<String> names = new LinkedList<>(personMap.keySet());
 
+        choice = (int) (Math.random() * 4);
 
+        Collections.shuffle(names);
 
-        String personX = names.get((int) ((names.size() + 1) * Math.random()));
-        //imageView.setImageBitmap();
+        button_0.setText(names.pop());
+        button_1.setText(names.pop());
+        button_2.setText(names.pop());
+        button_3.setText(names.pop());
 
-        switch ((int) (Math.random() * 4)) {
+        String keyName = "";
+
+        switch (choice) {
             case 0:
-                button_0.setText(personX);
+                keyName = button_0.getText().toString();
                 break;
             case 1:
-                button_1.setText(personX);
+                keyName = button_1.getText().toString();
                 break;
             case 2:
-                button_2.setText(personX);
+                keyName = button_2.getText().toString();
                 break;
             case 3:
-                button_3.setText(personX);
+                keyName = button_3.getText().toString();
                 break;
         }
 
+        ImageDownloader downloader = new ImageDownloader();
+        try {
+            imageView.setImageBitmap(downloader.execute("https://" + personMap.get(keyName)).get());
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -105,10 +135,71 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void onClickButton_0(View view) {
+        if (choice == 0) {
+            Toast.makeText(this, "RIGHT!", Toast.LENGTH_SHORT).show();
+            play();
+        }
+        else
+            Toast.makeText(this, "WRONG!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton_1(View view) {
+        if (choice == 1) {
+            Toast.makeText(this, "RIGHT!", Toast.LENGTH_SHORT).show();
+            play();
+        }
+        else
+            Toast.makeText(this, "WRONG!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton_2(View view) {
+        if (choice == 2) {
+            Toast.makeText(this, "RIGHT!", Toast.LENGTH_SHORT).show();
+            play();
+        }
+        else
+            Toast.makeText(this, "WRONG!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickButton_3(View view) {
+        if (choice == 3) {
+            Toast.makeText(this, "RIGHT!", Toast.LENGTH_SHORT).show();
+            play();
+        }
+        else
+            Toast.makeText(this, "WRONG!", Toast.LENGTH_SHORT).show();
+    }
+
+    private static class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+        private URL url = null;
+        private HttpsURLConnection httpsUrlConnections = null;
+        private Bitmap bitmap;
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            try {
+                url = new URL(strings[0]);
+                httpsUrlConnections = (HttpsURLConnection) url.openConnection();
+                InputStream in = httpsUrlConnections.getInputStream();
+                bitmap = BitmapFactory.decodeStream(in);
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (httpsUrlConnections != null) {
+                    httpsUrlConnections.disconnect();
+                }
+            }
+            return bitmap;
+        }
+    }
+
     private static class UrlTaskWorker extends AsyncTask<String, Void, String> {
         URL url = null;
         HttpsURLConnection httpsUrlConnection = null;
         InputStream in = null;
+
         @Override
         protected String doInBackground(String... strings) {
             StringBuilder strBld = new StringBuilder();
@@ -119,12 +210,12 @@ public class MainActivity extends AppCompatActivity {
                 InputStreamReader reader = new InputStreamReader(in, Charset.forName("UTF-8"));
                 int data = reader.read();
                 while (data != -1) {
-                    strBld.append((char)data);
+                    strBld.append((char) data);
                     data = reader.read();
                 }
                 reader.close();
 
-            } catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             } finally {
                 if (httpsUrlConnection != null)
